be44946d753b06db96955358cf7b3b85d7bed949
BasharHweij <bhweij@gmail.com>
2018-01-18 21:15:00 -0800

    First File

---
+++ Desktop/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    /* write 'Hello, world.' 10 times */
+    int i;
+    for (i=1; i <= 10; i++)
+    {
+        printf("Hello, world.\n");
+    }
+    return 0;
+}
+
